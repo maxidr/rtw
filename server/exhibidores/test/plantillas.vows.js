6ca92e8aacb23854d061fe56fbe2394f1909232a
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-01-30 00:19:14 -0300

    .

--- server/exhibidores/test/plantillas.vows.js
+++ server/exhibidores/test/plantillas.vows.js
@@ -5,6 +5,9 @@ var vows = require('vows'),
 
 app.listen(3000);
 
+var options = { body: 'tester' }
+console.log("body:" + options.body);
+
 var newbie = function(){ return tobi.createBrowser(3000, "localhost") };
 
 vows.describe('Plantillas').addBatch({
@@ -23,7 +26,7 @@ vows.describe('Plantillas').addBatch({
 		topic: function(){
 			var browser = newbie();
 			var data = JSON.stringify({body: 'id=12&template=%3Cp%3Etest%3C%2Fp%3E'});
-			browser.post('/plantillas', 'id=12&template=%3Cp%3Etest%3C%2Fp%3E' , this.callback);
+			browser.post('/plantillas', { body: 'id=12&template=%3Cp%3Etest%3C%2Fp%3E' } , this.callback);
 		},
 		'debe resultar correcto': function(res, $){
 			res.should.have.status(200);
